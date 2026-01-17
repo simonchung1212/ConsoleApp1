ece9f1363d4a974f771676e875e2f0f40853a999
chung <chungjenching@gmail.com>
2026-01-17 15:54:41 +0800

    簡化主程式僅顯示 Hello, World!
    移除所有互動式註解程式碼，僅保留輸出 "Hello, World!"，使程式更精簡。

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,2 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-//Console.WriteLine("What is your name?");
-//var name = Console.ReadLine();
-//var currentDate = DateTime.Now;
-//Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-//Console.Write($"{Environment.NewLine}Press any key to exit...");
-//Console.ReadKey(true);
-
