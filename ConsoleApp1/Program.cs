39568e66c654cde80dc2021b91223fb9e1ab7589
chung <chungjenching@gmail.com>
2026-01-17 15:51:22 +0800

    #10
    增加 Hello, World! 輸出次數並調整註解格式
    
    將原本單行 Hello, World! 改為輸出兩次，並將詢問姓名及顯示問候語的相關註解合併保留於同一行。

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 //Console.WriteLine("What is your name?");
 //var name = Console.ReadLine();
